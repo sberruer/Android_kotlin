055b264bf2c209b2e023330fc0dae92f78d57212
sebastien.berruer <sebastien.berruer@adventiel.fr>
2020-03-05 17:38:29 +0100

    feat(crashlytics): add some code to test Crashlytics

--- app/src/main/java/fr/sberruer/android_kotlin/MainActivity.kt
+++ app/src/main/java/fr/sberruer/android_kotlin/MainActivity.kt
@@ -1,12 +1,16 @@
 package fr.sberruer.android_kotlin
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.crashlytics.android.Crashlytics
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val crashBtn: Button = findViewById(R.id.crash_button)
+        crashBtn.setOnClickListener(View.OnClickListener { v -> Crashlytics.getInstance().crash() })
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
